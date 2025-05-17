48700c6004090ae3151d7a40da0942b86567f125
ysskrishna <sivasaikrishnassk@gmail.com>
2025-05-17 23:17:26 +0530

    Siva | get authentication token in frontend

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,11 +1,20 @@
+import { auth } from '@clerk/nextjs/server'
 import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const { getToken } = await auth()
+  const token = await getToken({ template: 'backend' })
+  console.log("token", token)
   return (
     <>
       <SignedIn>
-        <div className="flex min-h-screen items-center justify-center">
-          <h1 className="text-3xl font-bold">Welcome to your Dashboard!</h1>
+        <div className="flex min-h-screen items-center justify-center p-4">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold mb-4">Welcome to your Dashboard!</h1>
+            <p className="break-all max-w-2xl text-sm text-gray-600">
+              <span className="font-mono">{token}</span>
+            </p>
+          </div>
         </div>
       </SignedIn>
       <SignedOut>
@@ -13,4 +22,4 @@ export default function DashboardPage() {
       </SignedOut>
     </>
   );
-} 
+}
