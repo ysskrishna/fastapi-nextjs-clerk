33df750c1ee7d8c96752a476811f1bbd7ab3b3bc
ysskrishna <sivasaikrishnassk@gmail.com>
2025-05-18 21:04:35 +0530

    Siva | fix header and footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <footer className="w-full flex flex-col sm:flex-row items-center justify-between py-6 max-w-6xl mx-auto">
       <div className="text-sm text-gray-500  mb-2 sm:mb-0 text-center sm:text-left">
-        Made with <FaHeart className="inline h-4 w-4 text-black mx-1" /> by ysskrishna
+        <a href="https://bento.me/ysskrishna" target="_blank" rel="noopener noreferrer" aria-label="Website">
+          Made with <FaHeart className="inline h-4 w-4 text-black mx-1" /> by ysskrishna
+        </a>
       </div>
       <div className="flex space-x-6 justify-center sm:justify-end">
         <a href="https://bento.me/ysskrishna" target="_blank" rel="noopener noreferrer" aria-label="Website">
--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,7 +3,14 @@ import { UserButton } from "@clerk/nextjs";
 export default function Header() {
   return (
     <header className="w-full max-w-6xl flex flex-row justify-between items-center p-4 bg-white mx-auto">
-      <div className="text-xl font-bold text-gray-800">FastAPI + NextJS + Clerk Integration</div>
+      <a
+        href="https://github.com/ysskrishna/fastapi-nextjs-clerk"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-xl font-bold text-gray-800 hover:underline"
+      >
+        FastAPI + NextJS + Clerk Integration
+      </a>
       <UserButton afterSignOutUrl="/" />
     </header>
   );
